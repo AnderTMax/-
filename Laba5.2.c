aa2bdefbdc8ce1f7e3f9e100f128aa78feaf5172
Куртуков Антон <94165703+AnderTMax@users.noreply.github.com>
2021-12-22 01:49:37 +0500

    Add files via upload

---
+++ Laba5.2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float A, B, C;
+    printf("A:");
+    scanf_s("%f", &A);
+
+    printf("B:");
+    scanf_s("%f", &B);
+
+    printf("C:");
+    scanf_s("%f", &C);
+
+    float AC = abs(A - C);
+    printf("AC:%f\n", AC);
+
+    float BC = abs(B - C);
+    printf("BC:%f\n", BC);
+
+    printf("AC+BC:%f\n", BC + AC);
+
+    return 0;
+}
